44564e07336e91fd6a92379d3a857bec5d3689e4
Hadjira Bekhadda <88838686+hadjira21@users.noreply.github.com>
2025-04-24 00:14:39 +0200

    Update harvest_eco2mix_dag.py

--- dags/harvest/harvest_eco2mix_dag.py
+++ dags/harvest/harvest_eco2mix_dag.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.utils.dates import days_ago
-from repo.dags.operators.eco2mix_operator import Eco2mixDownloadOperator
+from dags.operators.eco2mix_operator import Eco2mixDownloadOperator
 
 default_args = {
     "owner": "airflow",
