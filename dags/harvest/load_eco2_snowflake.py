5461e9a31b072402b9932e75f6eaea5aa3588f7a
hadjira21 <88838686+hadjira21@users.noreply.github.com>
2025-06-15 23:59:13 +0200

    delete

--- dags/harvest/load_eco2_snowflake.py
+++ dags/harvest/load_eco2_snowflake.py
@@ -8,43 +8,6 @@ from airflow.operators.python import PythonOperator
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 from airflow.utils.dates import days_ago
 import logging
-# def upload_to_snowflake():
-#     conn_params = {'user': 'HADJIRA25', 'password' : '42XCDpmzwMKxRww', 'account': 'TRMGRRV-JN45028',
-#     'warehouse': 'COMPUTE_WH', 'database': 'BRONZE',  'schema': "RTE" }
-#     snowflake_hook = SnowflakeHook(
-#         snowflake_conn_id='snowflake_conn',
-#         **conn_params
-#     )
-
-#     # Utiliser la bonne base et schéma
-#     snowflake_hook.run(f"USE DATABASE {conn_params['database']}")
-#     snowflake_hook.run(f"USE SCHEMA {conn_params['schema']}")
-
-#     # Chemin du fichier CSV local
-#     file_path = '/opt/airflow/data/eCO2mix_RTE_En-cours-TR/eCO2mix_RTE_En-cours-TR.csv'
-#     stage_name = 'RTE_STAGE'
-
-#     # Upload fichier dans stage Snowflake (sans compression)
-#     put_command = f"PUT file://{file_path} @{stage_name}/eCO2mix_clean.csv.gz AUTO_COMPRESS=TRUE"
-#     snowflake_hook.run(put_command)
-
-#     # Copier les données depuis le stage vers la table Snowflake
-#     copy_query = """
-# COPY INTO eco2mix_data
-# FROM @METEO_STAGE/eCO2mix_RTE_En-cours-TR.csv.gz
-# FILE_FORMAT = (
-#     TYPE = 'CSV',
-#     FIELD_DELIMITER = '\t',
-#     FIELD_OPTIONALLY_ENCLOSED_BY = '"',
-#     DATE_FORMAT = 'YYYY-MM-DD',
-#     SKIP_HEADER = 1,
-#     ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE
-# )
-# ON_ERROR = 'CONTINUE';
-
-#     """
-
-#     snowflake_hook.run(copy_query)
 
 def upload_to_snowflake():
     conn_params = {'user': 'HADJIRA25', 'password' : '42XCDpmzwMKxRww', 'account': 'TRMGRRV-JN45028',
