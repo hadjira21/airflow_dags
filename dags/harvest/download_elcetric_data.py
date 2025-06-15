cb26174c92b4268166299136dd8bf40c0f4178c4
hadjira21 <88838686+hadjira21@users.noreply.github.com>
2025-06-15 11:34:08 +0200

    Update download_elcetric_data.py

--- dags/harvest/download_elcetric_data.py
+++ dags/harvest/download_elcetric_data.py
@@ -29,21 +29,21 @@ def read_data():
     except Exception as e:
         print(f"Erreur lors de la lecture du fichier CSV : {e}")
 
-# def upload_to_snowflake():
-#     conn_params = {'user': 'HADJIRABK', 'password' : '42XCDpmzwMKxRww', 'account': 'OKVCAFF-IE00559',
-#     'warehouse': 'COMPUTE_WH', 'database': 'BRONZE',  'schema': "ENEDIS" }
-#     snowflake_hook = SnowflakeHook( snowflake_conn_id='snowflake_conn', **conn_params)
-#     snowflake_hook.run(f"USE DATABASE {conn_params['database']}")
-#     snowflake_hook.run(f"USE SCHEMA {conn_params['schema']}")
-#     create_table_sql = """CREATE TABLE IF NOT EXISTS electric_data (
-#         Horodate STRING, Temp_realisee_lissee_C FLOAT, Temp_normale_lissee_C FLOAT,
-#         Diff_Temp_Realisee_Normale_C FLOAT,  Pseudo_rayonnement FLOAT,
-#         Annee INT,Mois INT, Jour INT, Annee_Mois_Jour STRING); """
-#     snowflake_hook.run(create_table_sql)
+def upload_to_snowflake():
+    conn_params = {'user': 'HADJIRABK', 'password' : '42XCDpmzwMKxRww', 'account': 'OKVCAFF-IE00559',
+    'warehouse': 'COMPUTE_WH', 'database': 'BRONZE',  'schema': "ENEDIS" }
+    snowflake_hook = SnowflakeHook( snowflake_conn_id='snowflake_conn', **conn_params)
+    snowflake_hook.run(f"USE DATABASE {conn_params['database']}")
+    snowflake_hook.run(f"USE SCHEMA {conn_params['schema']}")
+    create_table_sql = """CREATE TABLE IF NOT EXISTS electric_data (
+        Horodate STRING, Temp_realisee_lissee_C FLOAT, Temp_normale_lissee_C FLOAT,
+        Diff_Temp_Realisee_Normale_C FLOAT,  Pseudo_rayonnement FLOAT,
+        Annee INT,Mois INT, Jour INT, Annee_Mois_Jour STRING); """
+    snowflake_hook.run(create_table_sql)
 
 default_args = {"owner": "airflow", "start_date": datetime(2020, 3, 20), "retries": 0,}
 dag = DAG( "download_and_process_electrique_data", default_args=default_args, schedule_interval="@daily", catchup=False,)
 download_task = PythonOperator( task_id="download_csv_data", python_callable=download_data, dag=dag, )
 read_task = PythonOperator(task_id="read_csv_data", python_callable=read_data, dag=dag,)
-# upload_task = PythonOperator(task_id='upload_to_snowflake', python_callable=upload_to_snowflake, dag=dag)
-download_task >> read_task
+upload_task = PythonOperator(task_id='upload_to_snowflake', python_callable=upload_to_snowflake, dag=dag)
+download_task >> read_task >> upload_task
