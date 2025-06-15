677f44b7f44be796f05b69e08ec228ebf5ced79c
hadjira21 <88838686+hadjira21@users.noreply.github.com>
2025-06-15 23:08:25 +0200

    Update load_eco2_snowflake.py

--- dags/harvest/load_eco2_snowflake.py
+++ dags/harvest/load_eco2_snowflake.py
@@ -151,10 +151,12 @@ dag = DAG(
     start_date=datetime(2025, 6, 1),
     catchup=False,
 )
-load_task = PythonOperator(task_id='upload_to_snowflake',
-        python_callable=upload_to_snowflake,
-        provide_context=True,
-    )
+load_task = PythonOperator(
+    task_id='upload_to_snowflake',
+    python_callable=upload_to_snowflake,
+    provide_context=True,
+    dag=dag  
+)
 
 load_task
 
