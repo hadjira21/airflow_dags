1557168274a4147ce96006e97b7864c91c78b36d
hadjira21 <88838686+hadjira21@users.noreply.github.com>
2025-06-16 17:49:00 +0200

    update dbt dag

--- dags/harvest/dbt_test.py
+++ dags/harvest/dbt_test.py
@@ -1,22 +1,48 @@
 from airflow import DAG
-from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
-from datetime import datetime
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import requests
+import os
 
+# Paramètres DBT Cloud
+DBT_CLOUD_ACCOUNT_ID = 70471823469539
+DBT_CLOUD_JOB_ID = 70471837242727
+DBT_CLOUD_API_TOKEN = "dbtc_44q-vb47_gAXkzqbgVjeUY-RZk6LX3RZrY-tXwhPHg6uc-rv5w"
+
+DBT_TRIGGER_URL = f"https://cloud.getdbt.com/api/v2/accounts/{DBT_CLOUD_ACCOUNT_ID}/jobs/{DBT_CLOUD_JOB_ID}/run/"
+
+def trigger_dbt_cloud_job():
+    headers = {
+        "Authorization": f"Token {DBT_CLOUD_API_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(DBT_TRIGGER_URL, headers=headers)
+
+    if response.status_code != 200:
+        raise Exception(f"DBT Cloud job failed to trigger: {response.text}")
+    else:
+        run_id = response.json().get("data", {}).get("id")
+        print(f"DBT Cloud job triggered successfully, run_id: {run_id}")
+
+# DAG Airflow
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2025, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
 }
 
-with DAG(
-    dag_id='test_snowflake_connection',
+dag = DAG(
+    dag_id='trigger_dbt_silver_job',
     default_args=default_args,
-    schedule_interval=None,
+    description='Déclenche un job DBT Cloud pour les modèles Silver',
+    schedule_interval='0 6 * * *',  
+    start_date=datetime(2024, 1, 1),
     catchup=False,
-    tags=['test', 'snowflake'],
-) as dag:
-
-    test_query = SnowflakeOperator(
-        task_id='test_snowflake',
-        sql='SELECT current_version();',
-        snowflake_conn_id='snowflake_conn',
-    )
+    tags=['dbt', 'silver']
+)
+trigger_dbt = PythonOperator(
+        task_id='trigger_dbt_silver',
+        python_callable=trigger_dbt_cloud_job,
+        dag=dag    )
+trigger_dbt
