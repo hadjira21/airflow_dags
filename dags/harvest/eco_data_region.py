1cba6c7ae9c4c4a0578259b5f48a011923d0854d
hadjira21 <88838686+hadjira21@users.noreply.github.com>
2025-06-17 12:31:45 +0200

    Update eco_data_region.py

--- dags/harvest/eco_data_region.py
+++ dags/harvest/eco_data_region.py
@@ -118,8 +118,8 @@ def upload_to_snowflake(region, **kwargs):
     # Lecture CSV avec encodage latin1 et séparateur ';'
     df = pd.read_csv(file_paths['csv_file'], sep=';', encoding='ISO-8859-1')
     print(df.columns)
-    df = df[['Périmètre', 'Nature', 'Date', 'Heures', 'Consommation', 'Thermique', 'Eolien', 'Solaire', 'Hydraulique', 'Pompage']]
-    print(df.head())
+    # df = df[['Périmètre', 'Nature', 'Date', 'Heures', 'Consommation', 'Thermique', 'Eolien', 'Solaire', 'Hydraulique', 'Pompage']]
+    # print(df.head())
     # dtype_mapping = {
     #     'object': 'VARCHAR',
     #     'float64': 'FLOAT',
