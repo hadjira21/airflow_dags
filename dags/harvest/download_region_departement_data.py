bff29e8c7840ec0653e4017eaf51eb6c71e71068
hadjira21 <88838686+hadjira21@users.noreply.github.com>
2025-06-19 11:17:54 +0200

    Update download_region_departement_data.py

--- dags/harvest/download_region_departement_data.py
+++ dags/harvest/download_region_departement_data.py
@@ -10,7 +10,7 @@ DATA_DIR = "/opt/airflow/data"
 CSV_FILE = os.path.join(DATA_DIR, "departements.csv")
 CSV_URL = "https://www.data.gouv.fr/fr/datasets/r/70cef74f-70b1-495a-8500-c089229c0254"
 TABLE_NAME = "departements_francais"
-STAGE_NAME = "BRONZE_STAGE"
+STAGE_NAME = "ENEDIS_STAGE"
 
 def download_departements():
     os.makedirs(DATA_DIR, exist_ok=True)
@@ -35,7 +35,7 @@ def upload_departements_to_snowflake():
         'account': 'TRMGRRV-JN45028',
         'warehouse': 'INGESTION_WH',
         'database': 'BRONZE',
-        'schema': 'PUBLIC'
+        'schema': 'ENEDIS'
     }
 
     df = pd.read_csv(CSV_FILE, delimiter=',')
